48d0f65bfce89e1c02a680adb820a1d2b0939800
oleksiibabaikin <a.babaykin@gmail.com>
2022-12-21 21:31:15 +0200

    First version

---
+++ src/test002Serialazeble/Test004.java
@@ -0,0 +1,51 @@
+package test002Serialazeble;
+
+import java.io.*;
+
+public class Test004 {
+    public static void main(String[] args) {
+        System.out.println("main start");
+        Daemon daemon = new Daemon();
+        daemon.start();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("main end");
+    }
+}
+
+class Daemon extends Thread {
+
+    @Override
+    public void run() {
+        System.out.println("Daemon0 starts");
+        Daemon1 d = new Daemon1();
+      //  d.setDaemon(true);   //// TODO If you need Daemon
+        d.start();
+        System.out.println("Daemon0 ends");
+    }
+
+    class Daemon1 extends Thread {
+
+        @Override
+        public void run() {
+            System.out.println("Daemon1 - starts");
+            try {
+                Thread.sleep(4000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            try (FileOutputStream fo = new FileOutputStream(new File("d:\\12345678.txt"))) {
+                fo.write(1);
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Daemon1 - ends");
+        }
+    }
+}
